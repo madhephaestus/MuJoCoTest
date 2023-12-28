b662d6f386ba665ba611903aa9fac7227d05ea2a
hephaestus <hephaestus@zima-silver>
2023-12-28 13:03:21 -0500

    spaced prints

--- MuJoCoTest.groovy
+++ MuJoCoTest.groovy
@@ -16,19 +16,31 @@ if(!file.exists()) {
 	fail("File is missing from the disk");
 }
 MuJoCoModelManager m = new MuJoCoModelManager(file);
-def model = m.getModel();
-def data = m.getData();
-System.out.println("Run ModelManager for 10 seconds");
-IMujocoController controller =  {mjData_ d, mjModel_ mL->
-	// apply controls https://mujoco.readthedocs.io/en/stable/programming/simulation.html#simulation-loop
-	if( mL.nu()==mL.nv() )
-		MuJoCoLib.mju_scl(d.ctrl(), d.qvel(), -0.1, mL.nv());
-};
-m.setController(controller);
-while (data.time() < 10) {
-	m.step();
-	// sleep
-	Thread.sleep(m.getTimestepMilliSeconds());
-	println "Time: "+data.time()
+try {
+	def model = m.getModel();
+	def data = m.getData();
+	System.out.println("Run ModelManager for 10 seconds");
+	IMujocoController controller =  {mjData_ d, mjModel_ mL->
+		// apply controls https://mujoco.readthedocs.io/en/stable/programming/simulation.html#simulation-loop
+		if( mL.nu()==mL.nv() )
+			MuJoCoLib.mju_scl(d.ctrl(), d.qvel(), -0.1, mL.nv());
+	};
+	m.setController(controller);
+	long start = System.currentTimeMillis();
+	while (data.time() < 10  && !Thread.interrupted()) {
+		long now = System.currentTimeMillis()
+		m.step();
+		// sleep
+		Thread.sleep(m.getTimestepMilliSeconds());
+		if(now-start>100) {
+			start=now;
+			println "Time: "+data.time()
+		}
+	}
+}catch(Throwable t) {
+	t.printStackTrace(System.out);
 }
 m.close();
+
+
+
