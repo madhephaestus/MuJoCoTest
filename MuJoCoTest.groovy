450f03e77cb12683d1257cf54609267d899bfd10
hephaestus <hephaestus@zima-silver>
2023-12-28 12:45:38 -0500

    working example

--- MuJoCoTest.groovy
+++ MuJoCoTest.groovy
@@ -19,7 +19,7 @@ MuJoCoModelManager m = new MuJoCoModelManager(file);
 def model = m.getModel();
 def data = m.getData();
 System.out.println("Run ModelManager for 10 seconds");
-IMujocoController controller =  {d, mL->
+IMujocoController controller =  {mjData_ d, mjModel_ mL->
 	// apply controls https://mujoco.readthedocs.io/en/stable/programming/simulation.html#simulation-loop
 	if( mL.nu()==mL.nv() )
 		MuJoCoLib.mju_scl(d.ctrl(), d.qvel(), -0.1, mL.nv());
