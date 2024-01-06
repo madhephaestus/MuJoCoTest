f972d9be4f66bef3992ccb88f867ccf3f71262ad
Kevin Harrington <mad.hephaestus@gmail.com>
2024-01-06 14:04:42 -0500

    adding BNO style IMU

--- MujocoMobileBase.groovy
+++ MujocoMobileBase.groovy
@@ -20,6 +20,7 @@ import com.neuronrobotics.sdk.addons.kinematics.AbstractLink
 import com.neuronrobotics.sdk.addons.kinematics.DHParameterKinematics
 import com.neuronrobotics.sdk.addons.kinematics.LinkConfiguration
 import com.neuronrobotics.sdk.addons.kinematics.MobileBase
+import com.neuronrobotics.sdk.addons.kinematics.imu.IMUUpdate
 import com.neuronrobotics.sdk.addons.kinematics.math.RotationNR
 import com.neuronrobotics.sdk.addons.kinematics.math.TransformNR
 import com.neuronrobotics.sdk.common.DeviceManager
@@ -45,13 +46,15 @@ if(args!=null) {
 		return m;
 	})
 }
+String bodyName = cat.getScriptingName()+"_base"
+
 Thread.sleep(500);
 MobileBaseCadManager cadMan = MobileBaseCadManager.get(cat)
 HashMap<String,AbstractLink> linkNameMap = new HashMap<>()
 boolean viewer = cadMan.configMode
 while(cadMan.getProcesIndictor().get()<0.999) {
 	Thread.sleep(100);
-	println "Waiting for cad to process "+cadMan.getProcesIndictor().get() 
+	println "Waiting for cad to process "+cadMan.getProcesIndictor().get()
 }
 Thread.sleep(500);
 if(viewer) {
@@ -61,7 +64,7 @@ if(viewer) {
 }
 while(cadMan.getProcesIndictor().get()<0.999) {
 	Thread.sleep(100);
-	println "Waiting for cad to process "+cadMan.getProcesIndictor().get() 
+	println "Waiting for cad to process "+cadMan.getProcesIndictor().get()
 }
 
 double timestep = 0.005
@@ -69,31 +72,31 @@ String muXML = "<mujoco model=\""+cat.getScriptingName()+"\">\n";
 muXML+="  <option timestep=\""+timestep+"\"/>\n"
 muXML+="  <size njmax=\"8000\" nconmax=\"4000\"/>\n"
 muXML+="  <visual>\n"+
-		 "    <map force=\"0.1\" zfar=\"30\"/>\n"+
-		 "    <rgba haze=\"0.15 0.25 0.35 1\"/>\n"+
-		 "  </visual>\n"
+		"    <map force=\"0.1\" zfar=\"30\"/>\n"+
+		"    <rgba haze=\"0.15 0.25 0.35 1\"/>\n"+
+		"  </visual>\n"
 muXML+="  <statistic center=\"0 0 0.7\"/>\n"
-muXML+="  <asset>\n"+ 
-"    <texture type=\"skybox\" builtin=\"gradient\" rgb1=\".3 .5 .7\" rgb2=\"0 0 0\" width=\"32\" height=\"512\"/>\n"+ 
-"    <texture name=\"body\" type=\"cube\" builtin=\"flat\" mark=\"cross\" width=\"128\" height=\"128\" rgb1=\"0.8 0.6 0.4\" rgb2=\"0.8 0.6 0.4\" markrgb=\"1 1 1\" random=\"0.01\"/>\n"+ 
-"    <material name=\"body\" texture=\"body\" texuniform=\"true\" rgba=\"0.8 0.6 .4 1\"/>\n"+ 
-"    <texture name=\"grid\" type=\"2d\" builtin=\"checker\" width=\"512\" height=\"512\" rgb1=\".1 .2 .3\" rgb2=\".2 .3 .4\"/>\n"+ 
-"    <material name=\"grid\" texture=\"grid\" texrepeat=\"1 1\" texuniform=\"true\" reflectance=\".2\"/>\n"+ 
-"  </asset>\n"
+muXML+="  <asset>\n"+
+		"    <texture type=\"skybox\" builtin=\"gradient\" rgb1=\".3 .5 .7\" rgb2=\"0 0 0\" width=\"32\" height=\"512\"/>\n"+
+		"    <texture name=\"body\" type=\"cube\" builtin=\"flat\" mark=\"cross\" width=\"128\" height=\"128\" rgb1=\"0.8 0.6 0.4\" rgb2=\"0.8 0.6 0.4\" markrgb=\"1 1 1\" random=\"0.01\"/>\n"+
+		"    <material name=\"body\" texture=\"body\" texuniform=\"true\" rgba=\"0.8 0.6 .4 1\"/>\n"+
+		"    <texture name=\"grid\" type=\"2d\" builtin=\"checker\" width=\"512\" height=\"512\" rgb1=\".1 .2 .3\" rgb2=\".2 .3 .4\"/>\n"+
+		"    <material name=\"grid\" texture=\"grid\" texrepeat=\"1 1\" texuniform=\"true\" reflectance=\".2\"/>\n"+
+		"  </asset>\n"
 
 muXML+="  <default>\n"+
-		 "    <motor ctrlrange=\"-1 1\" ctrllimited=\"true\"/>\n"+
-		 "    <default class=\"body\">\n"+
-		 "\n"+
-		 "      <!-- geoms -->\n"+
-		 "      <geom type=\"sphere\" condim=\"1\" friction=\".7\" solimp=\".9 .99 .003\" solref=\".015 1\" material=\"body\" group=\"1\"/>\n"+
-		 "<!-- joints -->\n"+
-		 "      <joint type=\"hinge\" damping=\"7.5\" stiffness=\"5\" armature=\".01\" limited=\"true\" solimplimit=\"0 .99 .01\"/>\n"
+		"    <motor ctrlrange=\"-1 1\" ctrllimited=\"true\"/>\n"+
+		"    <default class=\"body\">\n"+
+		"\n"+
+		"      <!-- geoms -->\n"+
+		"      <geom type=\"sphere\" condim=\"1\" friction=\".7\" solimp=\".9 .99 .003\" solref=\".015 1\" material=\"body\" group=\"1\"/>\n"+
+		"<!-- joints -->\n"+
+		"      <joint type=\"hinge\" damping=\"7.5\" stiffness=\"5\" armature=\".01\" limited=\"true\" solimplimit=\"0 .99 .01\"/>\n"
 muXML+="    </default>\n"+
-		 "  </default>\n";
+		"  </default>\n";
 
 muXML+="  <worldbody>\n"+
-		 "    <geom name=\"floor\" size=\"0 0 .05\" type=\"plane\" material=\"grid\" condim=\"3\"/>\n"
+		"    <geom name=\"floor\" size=\"0 0 .05\" type=\"plane\" material=\"grid\" condim=\"3\"/>\n"
 muXML+=loadBase(cat,linkNameMap,cadMan)
 muXML+="\n  </worldbody>\n"
 
@@ -108,18 +111,18 @@ muXML+="\n</mujoco>"
 String loadBase(MobileBase m,HashMap<String,AbstractLink> map,MobileBaseCadManager cadMan) {
 	String name = m.getScriptingName()+"_base"
 	ArrayList<CSG>  cad = cadMan.getBasetoCadMap().get(m)
-	
-	CSG box =toBox(cad) 
+
+	CSG box =toBox(cad)
 	double x = box.getCenterX()/1000.0
-	
+
 	double y = box.getCenterY()/1000.0
-	
+
 	double z = box.getCenterZ()/1000.0
 	String XML ="    <body name=\""+name+"\" pos=\""+x+" "+y+" "+z+"\" childclass=\"body\">\n"
 	XML+="      <freejoint name=\"root\"/>\n"
 	CSG fbox = box.intersect(box.toXMin().movex(box.getCenterX()))
 	CSG bbox = box.intersect(box.toXMax().movex(box.getCenterX()))
-	
+
 	//XML+=csgToGeom(name+"_"+0,bbox,true)
 	int corners=1
 
@@ -127,15 +130,14 @@ String loadBase(MobileBase m,HashMap<String,AbstractLink> map,MobileBaseCadManag
 		if(k.getScriptingName().contains("Dummy"))
 			continue;
 		def toCSGTransformed = new Cube(10).toCSG()
-					.transformed(TransformFactory
-						.nrToCSG(k.getRobotToFiducialTransform()))
+				.transformed(TransformFactory
+				.nrToCSG(k.getRobotToFiducialTransform()))
 		XML+=csgToGeom(name+"_"+(corners++),toCSGTransformed,true)
-			
+
 		//if(k.getScriptingName().contentEquals("RightFront"))
-			XML+=loadLink(k,0,map,cadMan)
+		XML+=loadLink(k,0,map,cadMan)
 	}
 	XML+="    </body>\n"
- 
 }
 CSG toBox(ArrayList<CSG> cad) {
 	CSG box = null
@@ -167,7 +169,7 @@ int getLinkIndex(AbstractLink l, DHParameterKinematics k) {
 			return i;
 	}
 	return -1;
-} 
+}
 
 DHParameterKinematics getLimb(AbstractLink l, MobileBase m) {
 	for(DHParameterKinematics k:m.getAllDHChains()) {
@@ -202,27 +204,27 @@ String loadLink(DHParameterKinematics l,int index,HashMap<String,AbstractLink> m
 		location=new TransformNR(l.getDhLink(index-1).DhStep(0))
 	}
 	TransformNR local = new TransformNR(l.getDhLink(index).DhStep(0))
-//	if(index==0) {
-//		location=l.getRobotToFiducialTransform().copy()
-//	}else {
-//		location=l.forwardOffset(l.getLinkTip(index-1))
-//	}
-//	TransformNR lForwardOffset = l.forwardOffset(l.getLinkTip(index))
-//	TransformNR local = location.inverse().times(lForwardOffset)
-	
+	//	if(index==0) {
+	//		location=l.getRobotToFiducialTransform().copy()
+	//	}else {
+	//		location=l.forwardOffset(l.getLinkTip(index-1))
+	//	}
+	//	TransformNR lForwardOffset = l.forwardOffset(l.getLinkTip(index))
+	//	TransformNR local = location.inverse().times(lForwardOffset)
+
 	Transform step = TransformFactory.nrToCSG(local)
 	double x = location.getX()/1000.0
-	
+
 	double y = location.getY()/1000.0
-	
+
 	double z = location.getZ()/1000.0
 	String quat =" quat=\""+location.getRotation().getRotationMatrix2QuaturnionW()+" "+
-	location.getRotation().getRotationMatrix2QuaturnionX()+" "+
-	location.getRotation().getRotationMatrix2QuaturnionY()+" "+
-	location.getRotation().getRotationMatrix2QuaturnionZ()+"\""
+			location.getRotation().getRotationMatrix2QuaturnionX()+" "+
+			location.getRotation().getRotationMatrix2QuaturnionY()+" "+
+			location.getRotation().getRotationMatrix2QuaturnionZ()+"\""
 	String XML =spaces+"     <body name=\""+name+"\" pos=\""+x+" "+y+" "+z+"\" "+quat+">\n"
-	
-	TransformNR axis 
+
+	TransformNR axis
 	if(index==0) {
 		axis=l.getRobotToFiducialTransform().copy()
 	}else {
@@ -230,18 +232,18 @@ String loadLink(DHParameterKinematics l,int index,HashMap<String,AbstractLink> m
 	}
 
 	String pos = tfToPos(new TransformNR())
-	
+
 	double upper = link.getMaxEngineeringUnits()
 	double lower = link.getMinEngineeringUnits()
 	String range=lower+" "+upper
-	
+
 	XML+=spaces+"       <joint name=\""+name+"\" "+pos+" "+"range=\""+range+"\" />\n"
 	box=new Cube(10).toCSG()
 	XML+=spaces+" "+csgToGeom( name+"_A", box.transformed(step),true)
 	XML+=spaces+" "+csgToGeom( name+"_B", box,true)
-	
+
 	XML+=loadLink(l,index+1,map,cadMan)
-	
+
 	XML+=spaces+"     </body>\n"
 	//println XML
 	return XML
@@ -249,13 +251,13 @@ String loadLink(DHParameterKinematics l,int index,HashMap<String,AbstractLink> m
 
 String tfToPos(TransformNR tf) {
 	double x = 0
-	
+
 	double y = 0
-	
+
 	double z = 0
 	String STR=" pos=\""+x+" "+y+" "+z+"\""
 	TransformNR unit = new TransformNR(0,0,0,tf.inverse().getRotation())
-						.times(new  TransformNR(0,0,1,new RotationNR()))
+			.times(new  TransformNR(0,0,1,new RotationNR()))
 	STR+=" axis=\""+sig(unit.getX())+" "+sig(unit.getY())+" "+sig(unit.getZ())+"\""
 	return STR
 }
@@ -264,7 +266,7 @@ double sig(double x) {
 	if(x>0.999)
 		return 1;
 	if(x<-0.999)
-			return -1;
+		return -1;
 	if(x<0.001 && x>-0.001)
 		return 0
 	return x;
@@ -296,7 +298,7 @@ TransformNR convert(DoublePointer cartesianPositions,DoublePointer cartesianQuat
 	double x = coords.getPointer(0).get()*1000.0;
 	double y = coords.getPointer(1).get()*1000.0;
 	double z = coords.getPointer(2).get()*1000.0;
-	
+
 	DoublePointer quat =cartesianQuaturnions.getPointer(i*4);
 	double qw = quat.getPointer(0).get();
 	double qx = quat.getPointer(1).get();
@@ -304,9 +306,9 @@ TransformNR convert(DoublePointer cartesianPositions,DoublePointer cartesianQuat
 	double qz = quat.getPointer(3).get();
 	//if(print)
 	//println "coords "+[x,y,z]+" "+[qw, qx, qy, qz]
-	
+
 	RotationNR local = new RotationNR(qw, qx, qy, qz)
-	
+
 	return new TransformNR(x,y,z,local)
 }
 
@@ -333,7 +335,7 @@ try {
 	DoublePointer geomSize = model.geom_size()
 	DoublePointer geomPos = model.geom_pos()
 	DoublePointer geomQuat = model.geom_quat()
-	
+
 	IMujocoController controller =  {mjData_ d, mjModel_ mL->
 		/**
 		 * This illustrates two concepts. First, we are checking 
@@ -367,7 +369,7 @@ try {
 			//if(i!=1)continue;
 			String actName = modelNames.getPointer(acts.getPointer(i).get()).getString()
 			double position = Math.toDegrees(positions.get(actName))
-			
+
 			AbstractLink link = map.get(actName)
 			if(link==null)
 				continue;
@@ -375,15 +377,34 @@ try {
 			double error = posTarget-position
 			double effort = error * kp
 			ctrl.put(i, effort);
-			
+
 			//println actName+" "+i+" "+[position,posTarget]
 		}
 		positions.clear()
+		// Update the virtual IMU
+		for(int i=0;i<model.nbody();i++) {
+			if(bodyName.contentEquals(m.getBodyName(i))) {
+				DoublePointer cartesianQuaturnions = data.xquat();
+				DoublePointer cartesianPositions = data.xpos();
+				TransformNR tf= convert(cartesianPositions,cartesianQuaturnions,i,false)
+			
+				Double xAcceleration=Math.toDegrees(tf.getRotation().getRotationTilt());
+				Double yAcceleration=Math.toDegrees(tf.getRotation().getRotationAzimuth());
+				Double zAcceleration=Math.toDegrees(tf.getRotation().getRotationElevation());
+
+				Double rotxAcceleration=0;
+				Double rotyAcceleration=0;
+				Double rotzAcceleration=0;
+
+				cat.getImu().setVirtualState(new IMUUpdate( xAcceleration, yAcceleration, zAcceleration,
+						rotxAcceleration, rotyAcceleration, rotzAcceleration ))
+			}
+		}
 	};
 	m.setController(controller);
-	
 
-	
+
+
 	for(int i=0;i<model.nu();i++) {
 		IntPointer str = intp.getPointer(i);
 		BytePointer byp = new BytePointer();
@@ -408,34 +429,34 @@ try {
 		double x = coords.getPointer(0).get()*1000.0;
 		double y = coords.getPointer(1).get()*1000.0;
 		double z = coords.getPointer(2).get()*1000.0;
-		
+
 
 		int type = model.geom_type().getPointer(i).get()
 		/**
 		 *
-  mjGEOM_PLANE        = 0,        // plane
-  mjGEOM_HFIELD = 1,                  // height field
-  mjGEOM_SPHERE = 2,                  // sphere
-  mjGEOM_CAPSULE = 3,                 // capsule
-  mjGEOM_ELLIPSOID = 4,               // ellipsoid
-  mjGEOM_CYLINDER = 5,                // cylinder
-  mjGEOM_BOX = 6,                     // box
-  mjGEOM_MESH = 7,                    // mesh
+		 mjGEOM_PLANE        = 0,        // plane
+		 mjGEOM_HFIELD = 1,                  // height field
+		 mjGEOM_SPHERE = 2,                  // sphere
+		 mjGEOM_CAPSULE = 3,                 // capsule
+		 mjGEOM_ELLIPSOID = 4,               // ellipsoid
+		 mjGEOM_CYLINDER = 5,                // cylinder
+		 mjGEOM_BOX = 6,                     // box
+		 mjGEOM_MESH = 7,                    // mesh
 		 */
 		switch(type) {
 			case MuJoCoLib.mjGEOM_PLANE:
 				println "Plane ";
 				ball = new Cube(x==0?10000:x,y==0?10000:y,z).toCSG()
-							.toZMax()
-							ball.setColor(Color.WHITE)
+						.toZMax()
+				ball.setColor(Color.WHITE)
 				break;
 			case MuJoCoLib.mjGEOM_CAPSULE:
 				CSG top = new Sphere(x).toCSG()
 				ball = new Cylinder(x,y*2).toCSG()
-								.union(top)
-								.union(top.movez(y*2))
-								.hull()
-								.movez(-y)
+						.union(top)
+						.union(top.movez(y*2))
+						.hull()
+						.movez(-y)
 				break;
 			case MuJoCoLib.mjGEOM_SPHERE:
 				ball = new Sphere(x).toCSG()
@@ -445,15 +466,14 @@ try {
 		Transform nrToCSG = TransformFactory.nrToCSG(local)
 		ball=ball.transformed(nrToCSG)
 		ball.setManipulator(new Affine())
-		
+
 		ball.setName(bypGetString)
 		if(map.get(bodyID)==null)
 			map.put(bodyID,[])
 		map.get(bodyID).add(ball)
 	}
-	String bodyName = cat.getScriptingName()+"_base"
 	HashMap<String,AbstractLink> lnmap=linkNameMap
-	
+
 	HashMap<Affine,String> affineNameMap=[]
 	for(String s:lnmap.keySet()) {
 		Affine a = lnmap.get(s).getGlobalPositionListener()
@@ -472,7 +492,7 @@ try {
 			AbstractLink link = lnmap.get(name)
 			if(link!=null) {
 				LinkConfiguration conf = link.getLinkConfiguration()
-				
+
 				def array = cadMan.getLinktoCadMap().get(conf)
 				for(CSG c:array) {
 					if(!c.getStorage().getValue("no-physics").isPresent()) {
@@ -498,10 +518,10 @@ try {
 			}
 		}
 	}
-	
 
-	
-	
+
+
+
 	BowlerStudioController.clearCSG()
 	for(Integer i:map.keySet()) {
 		BowlerStudioController.addObject(map.get(i), null)
@@ -511,6 +531,7 @@ try {
 		long now = System.currentTimeMillis()
 		m.step();
 		// sleep
+
 		Thread.sleep(m.getTimestepMilliSeconds());
 		if(now-start>16) {
 			start=now;
@@ -522,9 +543,9 @@ try {
 				poss.add(convert(cartesianPositions,cartesianQuaturnions,i,false))
 			}
 			BowlerStudio.runLater({
-				
+
 				for(int i=0;i<model.nbody();i++) {
-					
+
 					TransformNR local = poss[i];
 					for(CSG bodyBall:map.get(i))
 						TransformFactory.nrToAffine(local, bodyBall.getManipulator())
